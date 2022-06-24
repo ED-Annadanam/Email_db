b83563b40e77f31dd736d04c62f72cb664977099
ED-Annadanam <engdesign12e@gmail.com>
2022-06-24 21:30:12 +0530

    csrf

--- myworld/settings.py
+++ myworld/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_heroku
 django_heroku.settings(locals())
+CSRF_TRUSTED_ORIGINS = [
+    'https://email-storage.herokuapp.com'
+]
